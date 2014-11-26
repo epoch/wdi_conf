ddf4e0d81d9f05470538f76a61dca1732ccd7012
drej2k <andrevidic@hotmail.com>
2014-11-27 10:17:46 +1100

    andres draft proposal (not working yet)

---
+++ Andre_Vidic/andre_talk_proposal.rb
@@ -0,0 +1,15 @@
+class Sublime
+
+	def intialize
+		"Andre's wdi_conf proposal on.."
+	end
+
+	def self.shortucts
+		"Sublime keyboard shortucts"
+		"-Where to find cool sublime colour schemes"
+		"-How to setting up colour toggle shortcut on sublime"
+	end
+end
+
+s1 = Sublime.new
+Sublime.shortcuts
