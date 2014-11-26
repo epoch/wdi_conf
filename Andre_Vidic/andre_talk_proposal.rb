3a8ff5fab73742a35f3cd92a4dfd7dd89e9a2f2f
drej2k <andrevidic@hotmail.com>
2014-11-27 10:23:41 +1100

    added Rob to the talk

--- Andre_Vidic/andre_talk_proposal.rb
+++ Andre_Vidic/andre_talk_proposal.rb
@@ -4,12 +4,21 @@ class Sublime
 		"Andre's wdi_conf proposal on.."
 	end
 
-	def self.shortucts
+	def shortucts
 		"Sublime keyboard shortucts"
+		"---------------------------"
+		"A joint talk with Rob Will"
+
+		"Topics to be covered"
+		"--------------------"
+
+		"Andre's Topics"
 		"-Where to find cool sublime colour schemes"
 		"-How to setting up colour toggle shortcut on sublime"
+
+		"Rob's Topics"
 	end
 end
 
 s1 = Sublime.new
-Sublime.shortcuts
+s1.shortcuts
