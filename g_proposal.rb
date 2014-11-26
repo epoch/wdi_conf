77d2be8ca551e7851de137beca45ca43f9a8f39f
Ji Lee <gafrong@gmail.com>
2014-11-27 10:31:48 +1100

    g's proposal

---
+++ g_proposal.rb
@@ -0,0 +1,15 @@
+class Proposal
+
+  def initialize
+    @buzz_words = ['Swing', 'Swipte', 'Flip Cards']
+  end
+
+  def talk_name
+    "Swiping selection"
+  end
+
+  def source
+    "https://github.com/gajus/swing"
+  end
+
+end
